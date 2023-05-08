ce3785b4036ed7c65593c677913eeae35066e4fb
AebDomenger <131205155+AebDomenger@users.noreply.github.com>
2023-05-08 11:37:30 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 1st function creates the storage system
+## 2nd function interrogates the storage system and completes it if needed
 
-## Write a short comment describing this function
+## Creates a matrix used to store in cache the inverted matrix of x once calculated
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  cached_matrix<-list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+       return(cached_matrix)
 }
 
 
-## Write a short comment describing this function
+## Check for the existence of x^(-1) in the cache and returns it if present, calculates it and stores in if not
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  s <- cached_matrix$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- cached_matrix$get()
+  s <- solve(data, ...)
+  cached_matrix$setsolve(s)
+  s
 }
